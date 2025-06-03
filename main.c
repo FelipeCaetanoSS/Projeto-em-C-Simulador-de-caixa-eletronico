354cafb59b40265c91359c2ce6603fa3a9f4e278
Felipe Caetano <126731108+FelipeCaetanoSS@users.noreply.github.com>
2025-06-03 11:41:21 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -7,7 +7,8 @@ int main()
     setlocale(LC_ALL, "Portuguese");
 
     int menu;
-
+    float saldo = 0;
+    float saque, deposito;
 
     printf("\t     BEM VINDO AO BANCO!!\n\n");
 
@@ -24,36 +25,56 @@ int main()
 
     switch (menu){
     case 0:
-        printf("--------------------------------------\n");
+        printf("\n--------------------------------------\n");
         printf("Encerrando Programa...\n\n\n\n");
             return 0;
     case 2:
-
-        printf("\nSALDO!!\n");
-        printf("--------------------------------------\n\n");
+        printf("\n\tSALDO!!\n\n");
+        printf("Seu saldo total é:\n", saldo);
+        printf("%.2f\n", saldo);
+        printf("\n--------------------------------------\n\n");
             break;
     case 3:
-
-        printf("\nSAQUE DE DINHEIRO!!\n");
-        printf("--------------------------------------\n\n");
+        printf("\n\tSAQUE DE DINHEIRO!!\n\n");
+        printf("Qual valor deseja retirar?\n");
+        scanf("%f", &saque);
+        if (saque > 0){
+            if (saque <= saldo){
+            saldo -= saque;
+        printf("Você acaba de retirar %.2f da sua conta.", saque);
+        } else{
+        printf("O valor não está disponível para resgate.");
+        }
+        } else{
+        printf("\nO valor não pode números negativos.");
+        }
+        printf("\n--------------------------------------\n\n");
             break;
     case 4:
+        printf("\n\tDEPOSITO DE DINHEIRO!!\n\n");
+        printf("Qual valor deseja depositar?\n");
+        scanf("%f", &deposito);
 
-        printf("\nDEPOSITO DE DINHEIRO!!\n");
-        printf("--------------------------------------\n\n");
+        if (deposito > 0){
+            saldo += deposito;
+        printf("\nSUCESSO!!!\n");
+        printf("Você acaba de depositar %.2f em sua conta.\n", deposito);
+        } else{
+        printf("\nO valor não pode números negativos.");
+        }
+        printf("\n--------------------------------------\n\n");
             break;
     case 5:
+        printf("\n\tEXTRATO!!\n\n");
 
-        printf("\nEXTRATO!!\n");
-        printf("--------------------------------------\n\n");
+        printf("\n--------------------------------------\n\n");
             break;
    default:
-        printf("\n\tOPÇÃO INVÁLIDA, TENTE NOVAMENTE.\n");
-        printf("--------------------------------------\n\n");
+        printf("\nOPÇÃO INVÁLIDA, TENTE NOVAMENTE.\n");
+        printf("\n--------------------------------------\n\n");
             break;
 }
     }
-
 
     return 0;
 }
